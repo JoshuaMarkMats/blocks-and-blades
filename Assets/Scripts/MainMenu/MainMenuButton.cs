b3ae2253be439aeb7a2afd90ea0b281999e221d4
gotme_Krackin <jeremyjosh.abad.21@usjr.edu.ph>
2023-11-27 01:55:19 +0800

    added main menu functionality
    can play and exit; settings pop-up works but cannot change audio

---
+++ Assets/Scripts/MainMenu/MainMenuButton.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public GameObject setting;
+    bool isOn = false;
+
+    public void play()
+    {
+        SceneManager.LoadScene(1);
+    }
+    
+    public void settings()
+    {
+        isOn = !isOn;
+        setting.SetActive(isOn);
+    }
+    
+    public void exit()
+    {
+        Application.Quit();
+    }
+
+
+
+}
