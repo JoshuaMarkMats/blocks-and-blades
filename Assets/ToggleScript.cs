c29e024a71050715d96eaa389874d36936f6edfa
straightman-boop <jeremyjosh.abad.21@usjr.edu.ph>
2023-11-30 00:45:34 -0800

    added pause functionality
    with UI and return to main menu

--- Assets/ToggleScript.cs
+++ Assets/ToggleScript.cs
@@ -1,20 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ToggleScript : MonoBehaviour
 {
     public GameObject GameObject;
+    public GameObject GameObject2;
     bool toggle = false;
+    static bool isPaused = false;
 
     void Update()
     {
         var a = Input.GetKeyDown(KeyCode.Tab);
+        var b = Input.GetKeyDown(KeyCode.Escape);
 
         if (a)
         {
             toggle = !toggle;
             GameObject.SetActive(toggle);
         }
+
+        if (b)
+        {
+            isPaused = !isPaused;
+            GameObject2.SetActive(isPaused);
+            Pause();
+        }
+    }
+
+    void Pause()
+    {
+        if (isPaused == true)
+        {
+            Time.timeScale = 0.0f;
+        }
+
+        else
+        {
+            Time.timeScale = 1.0f;
+        }
+
+    }
+
+    public void CloseBox()
+    {
+        if (GameObject2 == true)
+        {
+            isPaused = false;
+            Pause();
+            GameObject2.SetActive(false);
+        }
+    }
+
+    public void ReturnMainMenu()
+    {
+        SceneManager.LoadScene(0);
     }
 }
